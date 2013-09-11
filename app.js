22a887c4a748501cc339eb7ccd1956b95de710c5
Thomas Pinaud <pinaudt@gmail.com>
2013-09-11 16:17:14 +0200

    Added file structure. Added connection to the DB

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ var express = require('express');
 var app = express();
 
 var hbs = require('hbs');
+var mysql = require('mysql');
+
+var connection = require('./db/connection')(mysql);
 
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(express.bodyParser());
 
-var api = require('./api')(app);
+connection.query('SELECT * FROM `bookmarks`', function(err, rows, fields) {
+    if (err) throw err;
+
+    console.log('Result : ', rows[0]);
+});
+
+var api = require('./router/api')(app);
 
 app.listen(1337);
--- router/api.js
+++ router/api.js
@@ -17,4 +17,16 @@ module.exports = function(app) {
     app.get('/api/', function(req, res){
         res.json(getBookmarks());
     });
+
+    app.get('/api/bookmarks/', function(req, res){
+        res.json(getBookmarks());
+    });
+    app.get('/api/bookmarks/:id', function(req, res){
+        if(req.params.id && req.params.id > 0){
+
+        }else{
+            res.statusCode = 404;
+            return res.send('Error, parameter is not valid');
+        }
+    });
 }
