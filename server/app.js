38cddfdf8fc4121f900d5043fb57cfa0090157d4
Thomas Pinaud <pinaudt@gmail.com>
2013-09-16 20:49:55 +0200

    Updating gitignore. Resetting handlbars engine back

--- server/app.js
+++ server/app.js
@@ -3,15 +3,15 @@ var app = express();
 
 var hbs = require('hbs');
 
-app.use("/static", express.compress());
-app.use("/static", express.static("../client/public"));
-app.use("/static", function(req, res, next) {
-    res.send(404); // If we get here then the request for a static file is invalid
-  });
+//app.use("/static", express.compress());
+//app.use("/static", express.static("../client/public"));
+//app.use("/static", function(req, res, next) {
+    //res.send(404); // If we get here then the request for a static file is invalid
+//});
 
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
-//app.use(express.static('../client/public'));
+app.use(express.static('../client/public'));
 app.use(express.bodyParser());
 app.use(express.cookieParser());
 app.use(express.session({
@@ -30,7 +30,7 @@ var bookmark = require('./controller/api/bookmark')(app);
 
 app.all('/*', function(req, res) {
     // Just send the index.html for other files to support HTML5Mode
-    res.sendfile('index.html', {root: '../client'});
+    res.render('index');
 });
 
 app.listen(1337);
