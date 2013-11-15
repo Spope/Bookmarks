66ced2c467a176a721a6b3eaec73af5ce661f93b
Thomas Pinaud <pinaudt@gmail.com>
2013-11-15 12:05:08 +0100

    Database conversion loop on users

--- server/db/databaseConversion/conversion.php
+++ server/db/databaseConversion/conversion.php
@@ -3,7 +3,6 @@
 include('config.php');
 
 $connexion = connect_bd(SERVER,USER,PASSWORD,BASE_NAME);
-$idUser = 1;
 //
 function connect_bd($server, $user, $password, $base_name) {
     $connexion = mysql_connect($server,$user,$password)
@@ -15,47 +14,70 @@ function connect_bd($server, $user, $password, $base_name) {
     return $connexion;
 }
 
-$sql = "SELECT * FROM categorie WHERE user = ".$idUser." ORDER BY position";
 
-$result = mysql_query($sql);
-$categories = array();
-while($row = mysql_fetch_assoc($result)) {
-    array_push($categories, $row);
+//BEGIN
+$sql = "SELECT * FROM user";
+$resultUser = mysql_query($sql);
+
+
+while($row = mysql_fetch_assoc($resultUser)) {
+
+    $user = $row;
+
+    $connexion = connect_bd(SERVER,USER,PASSWORD,BASE_NAME);
+
+    $sql = "SELECT * FROM categorie WHERE user = ".$user['id']." ORDER BY position";
+
+    $result = mysql_query($sql);
+    $categories = array();
+    while($row = mysql_fetch_assoc($result)) {
+        array_push($categories, $row);
+    }
+
+    $sql = "SELECT * FROM bookmarks WHERE user = ".$user['id'];
+    $result = mysql_query($sql);
+    $bookmarks = array();
+    while($row = mysql_fetch_assoc($result)) {
+        array_push($bookmarks, $row);
+    }
+
+    mysql_close($connexion);
+
+    //////////////////
+    //////IMPORT
+    //////////////////
+
+    $connexion2 = connect_bd(SERVER_N,USER_N,PASSWORD_N,BASE_NAME_N);
+    //user
+    if($user['id'] == 1) {
+        $idUser = 1;
+    }else{
+        $sql = "INSERT INTO user (username, roles, created) VALUES ('".$user['login']."', 1, '".(date("Y-m-d H:i:s"))."')";
+        mysql_query($sql);
+        $idUser = mysql_insert_id();
+    }
+
+    //category
+    $categoryConversion = array();
+    foreach($categories as $k=>$v) {
+        $name = utf8_decode(html_entity_decode($v['nom']));
+        $sql = "INSERT INTO category (name, parent, user_id) VALUES ('".$name."', '".$v['idParent']."','".$idUser."')";
+        mysql_query($sql);
+        $categoryConversion[$v['id']] = mysql_insert_id();
+    }
+
+    //bookmark
+    $bookmarkConversion = array();
+    $sql = "SELECT id FROM category WHERE name = '__default' AND user_id = ".$idUser." LIMIT 1";
+    $result = mysql_query($sql);
+    $row = mysql_fetch_assoc($result);
+    $idFav = $row['id'];
+
+    convertB($bookmarks, $idUser);
+
 }
 
-$sql = "SELECT * FROM bookmarks WHERE user = ".$idUser;
-$result = mysql_query($sql);
-$bookmarks = array();
-while($row = mysql_fetch_assoc($result)) {
-    array_push($bookmarks, $row);
-}
-
-mysql_close($connexion);
-
-//////////////////
-//////IMPORT
-//////////////////
-
-//category
-$connexion2 = connect_bd(SERVER_N,USER_N,PASSWORD_N,BASE_NAME_N);
-$categoryConversion = array();
-foreach($categories as $k=>$v) {
-    $name = html_entity_decode($v['nom']);
-    $sql = "INSERT INTO category (name, parent, user_id) VALUES ('".$name."', '".$v['idParent']."','".$v['user']."')";
-    mysql_query($sql);
-    $categoryConversion[$v['id']] = mysql_insert_id();
-}
-
-//bookmark
-$bookmarkConversion = array();
-$sql = "SELECT id FROM category WHERE name = '__default' AND user_id = ".$idUser." LIMIT 1";
-$result = mysql_query($sql);
-$row = mysql_fetch_assoc($result);
-$idFav = $row['id'];
-
-convertB($bookmarks);
-
-function convertB($bookmarks) {
+function convertB($bookmarks, $idUser) {
 
     GLOBAL $bookmarkConversion;
     GLOBAL $categoryConversion;
@@ -69,7 +91,7 @@ function convertB($bookmarks) {
             $name = html_entity_decode($v['nom']);
 
             $sql = "INSERT INTO bookmark (name, url, position, parent, user_id, category_id, bookmark_type_id) 
-                VALUES ('".$name."', '".$v['url']."','".$v['position']."',".$idParent.", '".$v['user']."', '".$idCategory."', '".($v['type']+1)."')";
+                VALUES ('".$name."', '".$v['url']."','".$v['position']."',".$idParent.", '".$idUser."', '".$idCategory."', '".($v['type']+1)."')";
 
             mysql_query($sql);
 
@@ -81,7 +103,7 @@ function convertB($bookmarks) {
 
     if(count($bookmarks) > 0) {
 
-            convertB($bookmarks);
+            convertB($bookmarks, $idUser);
         }
 }
 
