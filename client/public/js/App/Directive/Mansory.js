afa5ac10c9dc22a3daf959efcaf09078f2bb4240
Thomas Pinaud <pinaudt@gmail.com>
2013-11-19 16:45:03 +0100

    Removed console.log

--- client/public/js/App/Directive/Mansory.js
+++ client/public/js/App/Directive/Mansory.js
@@ -15,7 +15,6 @@ directives.directive("mansory", [ '$timeout', function($timeout){
             }
 
             scope.mansory = function() {
-                console.log('mansory');
                 //Hack to wait the render to finish
                 $timeout(function(){
                     if(element.hasClass('isotope')){
