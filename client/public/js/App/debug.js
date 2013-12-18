afd2472864f02891bf3e29c4fb056e596a241af8
Thomas Pinaud <pinaudt@gmail.com>
2013-12-18 11:45:36 +0100

    Added ugly tweak to debug mode which synchronously load the templates folder / bookmark

---
+++ client/public/js/App/debug.js
@@ -0,0 +1,31 @@
+//================================================
+// Force to synchronously load the 2 templates
+//================================================
+
+services.service('loadConfigTemplate').run(['$templateCache', '$http', function($templateCache, $http){
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "js/App/View/Bookmarks/bookmark.html", false);
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                $templateCache.put('js/App/View/Bookmarks/bookmark.html', xhr.responseText)
+            } else {
+            }
+        }
+    };
+    xhr.send(null);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "js/App/View/Bookmarks/folder.html", false);
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                $templateCache.put('js/App/View/Bookmarks/folder.html', xhr.responseText)
+            } else {
+            }
+        }
+    };
+    xhr.send(null);
+
+}])
